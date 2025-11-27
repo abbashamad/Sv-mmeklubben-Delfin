650047f17d3c72d1d4b109ea0aa93d928fb39d6f
kasper <kasperj2016@gmail.com>
2025-11-27 10:58:21 +0100

    Member + MemberList + EliteMember - KC, TA

--- src/Member.java
+++ src/Member.java
@@ -1,13 +1,13 @@
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 public class Member {
-    private LocalDateTime birthday;
+    private LocalDate birthday;
     private int id;
     private String memberType;
     private String email;
     private String name;
 
-    public Member(LocalDateTime birthday, int id, String memberType, String email, String name) {
+    public Member(LocalDate birthday, int id, String memberType, String email, String name) {
         this.birthday = birthday;
         this.id = id;
         this.memberType = memberType;
@@ -15,7 +15,7 @@ public class Member {
         this.name = name;
     }
 
-    public LocalDateTime getBirthday() {
+    public LocalDate getBirthday() {
         return birthday;
     }
 
