36fd48d9c74f31ccd9f9b43d7de6d27baf6fb063
RaxosZero <radbob12@gmail.com>
2025-11-27 14:33:25 +0100

    Testing Commit Success (No Changes)

--- src/EliteMember.java
+++ src/EliteMember.java
@@ -4,21 +4,21 @@ import java.util.ArrayList;
 
 public class EliteMember extends Member {
 
-ArrayList <SwimResults> swimResults;
+    ArrayList<SwimResults> swimResults;
 
     public EliteMember(LocalDate birthday, int id, String email, String name) {
         super(birthday, id, email, name);
 
-       this.swimResults = new ArrayList<>();
+        this.swimResults = new ArrayList<>();
 
     }
 
-    public void addSwimResultsToList(LocalDate swimDate, LocalDateTime swimTime, Disciplines disciplines){
-        this.swimResults.add(new SwimResults(disciplines,swimTime, swimDate));
+    public void addSwimResultsToList(LocalDate swimDate, LocalDateTime swimTime, Disciplines disciplines) {
+        this.swimResults.add(new SwimResults(disciplines, swimTime, swimDate));
     }
 
-    public void addCompetitionResultToList(Disciplines discipline, LocalDateTime time, LocalDate date, int placement, String location){
-        this.swimResults.add(new StaevneTimeReg(discipline,time, date, placement,location));
-}
+    public void addCompetitionResultToList(Disciplines discipline, LocalDateTime time, LocalDate date, int placement, String location) {
+        this.swimResults.add(new StaevneTimeReg(discipline, time, date, placement, location));
+    }
 
 }
