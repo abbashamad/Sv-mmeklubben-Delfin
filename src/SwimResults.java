67aebf4ff0ded27988641c7454e9c5e0a24dcf88
RaxosZero <radbob12@gmail.com>
2025-11-27 12:09:06 +0100

    +SwimResult.java

---
+++ src/SwimResults.java
@@ -0,0 +1,35 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class SwimResults {
+    Member member;
+    Disciplines disciplines;
+    private LocalDateTime swimTime;
+    private LocalDate swimDate;
+
+    public SwimResults( LocalDate swimDate, LocalDateTime swimTime, Disciplines disciplines) {
+        this.swimDate = swimDate;
+        this.swimTime = swimTime;
+        this.disciplines = disciplines;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public Disciplines getDisciplines() {
+        return disciplines;
+    }
+
+
+    public LocalDateTime getSwimTime() {
+        return swimTime;
+    }
+
+
+    public LocalDate getSwimDate() {
+        return swimDate;
+    }
+
+
+}
