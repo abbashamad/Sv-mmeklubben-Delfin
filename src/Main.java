649cd34c488ea9d5f5efeafae97d26f8a5aa7b45
Malthe Rud <maltherud@gmail.com>
2025-12-01 11:36:55 +0100

    Har rettet det tilbage i member

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,6 @@ public class Main {
 
 
         MemberList list = new MemberList();
-        list.addMemberToMemberList(LocalDate.of(2000, 11, 15), "kasper@@@", "Søren");
-
-
-        list.addEliteToMemberList(LocalDate.of(2000,10,15) ,"kasperper@@" , "Sisse" );
-        System.out.println(list.findMemberViaID(2));
-        EliteMember eleteme = (EliteMember)list.findMemberViaID(2);
-        eleteme.addSwimResultsToList(LocalDate.of(2004,11,5), new SwimTimer(1,22,44), Discipline.BACKCRAWL);
-
-
         MenuSystem menu = new MenuSystem(list);
 
         menu.showMainMenu();
